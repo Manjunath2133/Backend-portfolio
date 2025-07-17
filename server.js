d394354d86d98502963bf4c037eb57622ca810ae
Manjunath2133 <kmanjunath0017@gmail.com>
2025-07-17 20:04:21 +0530

    Add dotenv config and use env variables for port

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ import bodyParser from 'body-parser';
 import contactRoutes from './routes/contact.js';
 import authRoutes from './routes/auth.js';
 import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
 
-
+dotenv.config();
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 
 app.use(cors({
@@ -21,6 +22,6 @@ app.use(cookieParser());
 app.use('/api/contact', contactRoutes);
 app.use('/', authRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
