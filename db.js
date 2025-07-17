ee5900c1244ce11e86e18fe356e6856db955b969
Manjunath2133 <kmanjunath0017@gmail.com>
2025-07-17 21:09:51 +0530

    updated postgrees with the Render

--- db.js
+++ db.js
@@ -1,15 +1,36 @@
+// import pg from 'pg';
+// const { Pool } = pg;
+
+// const pool = new Pool({
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'portfolio',
+//   password: '1234',
+//   port: 5432,
+//   // ssl: {
+//   //   rejectUnauthorized: false, // Needed for Render
+//   // }
+// });
+
+// export default pool;
+
+// db.js
 import pg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const { Pool } = pg;
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'portfolio',
-  password: '1234',
-  port: 5432,
-  // ssl: {
-  //   rejectUnauthorized: false, // Needed for Render
-  // }
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  ssl: {
+    rejectUnauthorized: false, // Required by Render
+  },
 });
 
 export default pool;
